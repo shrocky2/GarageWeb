d401d9ed2837234f63da9b8b6b5b0dc9e5368cca
Nicholas Keown <nicholaskeown@gmail.com>
2021-07-24 18:54:44 +1000

    encapsulate what varies. Make endpoints lowercase

--- web.py
+++ web.py
@@ -5,76 +5,53 @@ from flask import Flask, render_template, request
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
 GPIO.setwarnings(False)
-GPIO.setup(16, GPIO.IN, GPIO.PUD_UP) # set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
-GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) # set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(16, GPIO.IN, GPIO.PUD_UP) # set pin 16 as input and pull up to 3V via software pull-up resistor
+GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) # set pin 18 as input and pull up to 3V via software pull-up resistor
+GPIO.setup(7, GPIO.OUT) # output relay trigger 1
 GPIO.output(7, GPIO.HIGH)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT) # output relay trigger 2
 GPIO.output(11, GPIO.HIGH)
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT) # output relay trigger 3
 GPIO.output(13, GPIO.HIGH)
-GPIO.setup(15, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT) # output relay trigger 4
 GPIO.output(15, GPIO.HIGH)
 
 
-
+# Change default password here. '12345678' is the default Password that Opens Garage Door.
+passwd = '12345678'
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
-             print("Garage is Opening/Closing")
-             return app.send_static_file('Question.html')
-        else:  
-             if GPIO.input(16) == GPIO.LOW:
-                   print ("Garage is Closed")
-                   return app.send_static_file('Closed.html')
-             if GPIO.input(18) == GPIO.LOW:
-                   print ("Garage is Open")
-                   return app.send_static_file('Open.html')
-
-
-@app.route('/Garage', methods=['GET', 'POST'])
-def Garage():
+    if  request.method == 'POST':       
         name = request.form['garagecode']
-        if name == '12345678':  # 12345678 is the Password that Opens Garage Door (Code if Password is Correct)
-                GPIO.output(7, GPIO.LOW)
-                time.sleep(1)
-                GPIO.output(7, GPIO.HIGH)
-                time.sleep(2)
+        if name == passwd:
+            # password correctly entered - toggle "button press"
+            GPIO.output(7, GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(7, GPIO.HIGH)
+            time.sleep(2)
+        else:
+            if name == "":
+                name = "[empty]"
+            print("Garage Code Entered: " + name)
 
-                if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
-                  print("Garage is Opening/Closing")
-                  return app.send_static_file('Question.html')
-                else:
-                  if GPIO.input(16) == GPIO.LOW:
-                        print ("Garage is Closed")
-                        return app.send_static_file('Closed.html')
-                  if GPIO.input(18) == GPIO.LOW:
-                        print ("Garage is Open")
-                        return app.send_static_file('Open.html')
-
-        if name != '12345678':  # 12345678 is the Password that Opens Garage Door (Code if Password is Incorrect)
-                if name == "":
-                        name = "NULL"
-                print("Garage Code Entered: " + name)
-                if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
-                  print("Garage is Opening/Closing")
-                  return app.send_static_file('Question.html')
-                else:
-                  if GPIO.input(16) == GPIO.LOW:
-                        print ("Garage is Closed")
-                        return app.send_static_file('Closed.html')
-                  if GPIO.input(18) == GPIO.LOW:
-                        print ("Garage is Open")
-                        return app.send_static_file('Open.html')
+    if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
+        print("Garage is Opening/Closing")
+        return app.send_static_file('Question.html')
+    elif GPIO.input(16) == GPIO.LOW:
+        print ("Garage is Closed")
+        return app.send_static_file('Closed.html')
+    elif GPIO.input(18) == GPIO.LOW:
+        print ("Garage is Open")
+        return app.send_static_file('Open.html')
 
 @app.route('/stylesheet.css')
 def stylesheet():
         return app.send_static_file('stylesheet.css')
 
-@app.route('/Log')
+@app.route('/log')
 def logfile():
         return app.send_static_file('log.txt')
 
